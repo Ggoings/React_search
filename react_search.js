8d638ec25ef8776bd84b6ce47668422df2b13049
Ggoings <42050294+Ggoings@users.noreply.github.com>
2019-01-29 20:19:40 -0600

    Add files via upload

---
+++ react_search.js
@@ -0,0 +1,7 @@
+When you navigate to the app home page, you will see an article search form with three input fields. 
+
+Enter the topic you want to search for in the Topic field, a start year in the Start year field, and an end year in the End year field. 
+
+After you provide that information, click Search. When you click Search, a list of articles about your topic from the years specified will be retrieved using the New York Times Article Search API. 
+
+If you want to read the entire article, click Continue reading, which is an external link that takes you to the full length article on the New York Times website.
